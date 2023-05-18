ac8a2d10233fbf0fad54a31d13f832c505aac08a
labib <halalulislamsohel@gmil.com>
2023-05-18 12:01:35 +0600

    18 may 2023

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,7 @@
-alert()
+console.log(1000);
+console.warn('hello world');
+console.error('hello world');
+console.dir(5000);
+console.info('aksah');
+
+console.log(confirm('apni ki amar satha jaban'))
